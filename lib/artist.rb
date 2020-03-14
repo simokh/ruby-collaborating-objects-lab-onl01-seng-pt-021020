7a4e3fd31df08b4262491947250586724afcb5c5
Simo Khouili <m.simokhouili@gmail.com>
2020-03-14 20:02:20 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+require 'pry'
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+
+
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end
