9be98aa108af2b4a2ef62bc3f411391e57e8eee1
Simo Khouili <m.simokhouili@gmail.com>
2020-03-14 19:37:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs 
 
   @@all = []
 
@@ -14,5 +14,15 @@ class Artist
     @@all
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-end 
+
+
+  end
+
+
+end
